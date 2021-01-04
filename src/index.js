e3faaeecb3dac4a9f9ad2bb8455cd9ebaa012dbd
Dmg5th <dmgould310@gmail.com>
2021-01-04 14:35:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-const taskForm = document.querySelector("#create-task-form")
-const taskList = document.querySelector("#tasks")
+const taskForm = document.querySelector("#create-task-form");
+const taskList = document.querySelector("#tasks");
 
 taskForm.addEventListener("submit", function(e){
   e.preventDefault();
+
   const newTask = document.querySelector("#new-task-description").value
-  const taskItem = document.createElement("li");
 
-  taskList.innerHTML +=
-    `<li> ${newTask} cool!
-    <button data-action="delete">Delete</button></li>
-    `
-  
-  // taskItem.innerText = newTask
+  const taskItem = document.createElement("li")
 
-  // taskList.appendChild(taskItem);
-  
-  taskForm.reset()
+  taskList.innerHTML += 
+      `<li> ${newTask}
+        <button data-action= "delete">DELETE</button>
+      </li>`
+
+  taskForm.reset();
 })
 
 taskList.addEventListener("click", function(e){
-  console.log(e.target)
-  if (e.target.dataset.action === "delete"){
+  if (e.target.dataset.action === "delete") {
     e.target.parentElement.remove()
   }
 })
-
 
 
